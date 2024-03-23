f22a7d4b4030545eb7aa073ff0eb9fbe65533095
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-03-23 01:03:05 +0000

    get function is correct, not done with post

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,58 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
+	function fetchTodo (){
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/loganhavens', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // Will be true if the response is successful
+        console.log(resp.status); // The status code=200 or code=400 etc.
+        return resp.json(); 
+		// (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+    })
+    .then(data => {
+        setTodos (data)
+        console.log(data); // This will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        // Error handling
+        console.log(error);
+		console.log("hey")
+    });
+	};
+
+	function postTodo (){
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/loganhavens', {
+  method: 'POST', 
+  body: JSON.stringify(todos), 
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+  .then(res => {
+    if (!res.ok) throw Error(res.statusText);
+    return res.json();
+  })
+  .then(response => console.log('Success:', response))
+  .catch(error => console.error(error));
+	}
+
+  
+	useEffect(()=>{
+		fetchTodo()
+	}, []) 
+
+	useEffect (()=>{
+		postTodo
+	}, [])
+
 	return (
 		<div className="containter">
 			<h1>To Do's</h1>
@@ -26,7 +72,7 @@ const Home = () => {
 				</li>
 				{todos.map((item, index) =>(
 					<li>
-					{item} <i class="fa fa-trash" aria-hidden="true" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+					{item.label} <i class="fa fa-trash" aria-hidden="true" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
 					</li>
 				))}
 			</ul>
